2f2e698d632ba54efcdf1da10227e3f93f48aa8f
Сергей Голованов <golovanovsv16071975@gmail.com>
2020-11-09 18:15:22 +0500

    NASM - page 42

--- hello5.asm
+++ hello5.asm
@@ -1,11 +1,11 @@
 %include "stud_io.inc"
-global  _start
+global  _start				; start point
 
 section .text
-_start: mov	eax, 0
-again:  PRINT	"Hello"
-	PUTCHAR 10
-	inc	eax
-	cmp	eax, 5
-	jl	again
-	FINISH
+_start: mov	eax, 0			; zero eax
+again:  PRINT	"Hello, World!"		; print string
+	PUTCHAR 10			; new string
+	inc	eax			; increment eax
+	cmp	eax, 5			; compare eax with 5
+	jl	again			; if eax lower 5 - jump label again
+	FINISH				; end programm
